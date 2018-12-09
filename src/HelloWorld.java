629337e3ce51b90c6bfba1406223fcaff88d7771
divaibhav <divaibhav@gmail.com>
2018-12-09 14:09:22 +0530

    Added void show(String username)to display hello world to user.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -10,8 +10,16 @@ public class HelloWorld {
         username=sc.nextLine();
         //displaying user name with hello
         // to display value of variable use variable name
-        System.out.println("Hello,"+ username);
+        //System.out.println("Hello,"+ username);
+
+        //now we will use funtions to display user hello world to user
+        show(username);
+        sc.close();
 
 
+
+    }
+    public static void show(String username){
+        System.out.println("Hello World,"+ username);
     }
 }
