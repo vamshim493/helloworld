543a64ae44e7e163d4975c2a7f90bcf7c4ebb9a7
divaibhav <divaibhav@gmail.com>
2018-12-09 13:56:18 +0530

    updated user input and showing user name

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World");
+    public static void main(String[] args){
+        // to read input from user
+        Scanner sc=new Scanner(System.in);
+        //to store user name
+        String username;
+        //reading and storing user name
+        username=sc.nextLine();
+        //displaying user name with hello
+        // to display value of variable use variable name
+        System.out.println("Hello,"+ username);
+
+
     }
 }
